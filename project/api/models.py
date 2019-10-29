bb966bf7055a9bf3ce74cfbe026acd10f47c2825
Lucas Dutra <ldutra98@gmail.com>
2019-10-29 01:01:46 -0300

    pax-app/Wiki#189 Create PAX models and its methods

--- project/api/models.py
+++ project/api/models.py
@@ -5,5 +5,34 @@ from database_singleton import Singleton
 
 db = Singleton().database_connection()
 
+
 class Pax(db.Model):
     __tablename__ = 'PAX'
+
+    pax_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    data = db.Column(db.Date, nullable=False)
+    description = db.Column(db.String(500), nullable=False)
+    name = db.Column(db.String(255), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    status = db.Column(db.Enum('F', 'P', 'C', 'I'), nullable=False)
+    user_id = db.Column(db.Integer, nullable=False)
+    provider_id = db.Column(db.Integer, nullable=False)
+    chat_id = db.Column(db.Integer, nullable=False)
+    address_id = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, data, description, name, price, status):
+        self.data = data
+        self.description = description
+        self.name = name
+        self.price = price
+        self.status = name
+
+    def to_json(self):
+        return {
+            'id': self.pax_id,
+            'data': self.data,
+            'description': self.description,
+            'name': self.name,
+            'price': self.price,
+            'status': self.status,
+        }
