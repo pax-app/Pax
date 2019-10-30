dff96f1bd289403fac6966b7652bf5cc6bb8ab86
Lucas Dutra <ldutra98@gmail.com>
2019-10-30 01:02:31 -0300

    pax-app/Wiki#189 Update attributes from parameters reference

--- project/api/utils/status_strategy.py
+++ project/api/utils/status_strategy.py
@@ -19,36 +19,36 @@ class Context():
     def strategy(self, strategy: Strategy) -> None:
         self._strategy = strategy
 
-    def execute_filtering(self, id) -> list:
-        result = self._strategy.filter_status(id)
+    def execute_filtering(self, user_type, id) -> list:
+        result = self._strategy.filter_status(user_type, id)
         return result
 
 
 class Strategy(ABC):
     @abstractmethod
-    def filter_status(self, id):
+    def filter_status(self, user_type, id):
         pass
 
 
 class InitiatedStrategy(Strategy):
     def filter_status(self, user_type, id) -> list:
-        pax = utils.filter_by_status('I', self.user_type, self.id)
+        pax = utils.filter_by_status('I', user_type, id)
         return pax
 
 
 class CanceledStrategy(Strategy):
     def filter_status(self, user_type, id) -> list:
-        pax = utils.filter_by_status('C', self.user_type, self.id)
+        pax = utils.filter_by_status('C', user_type, id)
         return pax
 
 
 class PendentStrategy(Strategy):
     def filter_status(self, user_type, id) -> list:
-        pax = utils.filter_by_status('P', self.user_type, self.id)
+        pax = utils.filter_by_status('P', user_type, id)
         return pax
 
 
 class FinalizedStrategy(Strategy):
     def filter_status(self, user_type, id) -> list:
-        pax = utils.filter_by_status('F', self.user_type, self.id)
+        pax = utils.filter_by_status('F', user_type, id)
         return pax
